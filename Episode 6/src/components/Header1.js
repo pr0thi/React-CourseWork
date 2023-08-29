6a79529a6398f4dfd97f88d5e02e67412f0aee99
Harsh Pruthi <68766440+pr0thi@users.noreply.github.com>
2023-08-29 21:45:54 +0530

    Add files via upload

---
+++ Episode 6/src/components/Header1.js
@@ -0,0 +1,31 @@
+import { LOGO_URL } from "../utils/constants";
+
+const Header = () => {
+
+    let btnName = "Login";
+
+
+    return (
+        <div className="header">
+            <div className="logo-container">
+                <img className="logo" src={LOGO_URL} />
+            </div>
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>ContactUs</li>
+                    <li>Cart</li>
+                    <button className="login" onClick={()=>{
+                        btnName="Logout";
+                        console.log(btnName);
+                    }}>{btnName}</button>
+                </ul>
+            </div>
+
+        </div>
+    );
+};
+
+export default Header;
+ 
